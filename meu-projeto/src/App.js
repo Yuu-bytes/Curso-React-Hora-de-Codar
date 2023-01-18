e54a834c1e08cbf2c897c5b32f5d716c1417fe0b
Michael Trage <osuperyuu@gmail.com>
2023-01-17 22:36:43 -0300

    Aula 03 - Entendendo o JSX - Nesta aula foi aprendido sobre o uso de variáveis dentro do React, sobre o uso de funções, sobre como o nome das classes no React muda para className e como o método de return deve retornar um elemento apenas sempre.

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
 
 function App() {
+
+    const name = 'Michael';
+
+    const newName = name.toUpperCase();
+
+    function sum(a, b) {
+        return a + b;
+    }
+
+    const url = 'https://via.placeholder.com/150';
+
     return (
         <div className="App">
-            <h1>Olá Mundo! Olá React!</h1>
-            <p>Meu primeiro App</p>
+            <h2>Alterando o JSX</h2>
+            <p>Olá, {newName}</p>
+            <p>A soma de 3 + 2 é: {sum(3, 2)}</p>
+            <img src={url} alt="Minha Imagem" />
         </div>
     );
 }
