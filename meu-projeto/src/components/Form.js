58130f55575cc2f2b1fcb36a8ee6c7eed019c804
Michael Trage <osuperyuu@gmail.com>
2023-01-31 22:05:37 -0300

    Aula 10 - useState na pratica - Nesta aula foi aprendido a utilizar o useState para salvar e alterar o valor pegos de campos em eventos.

--- meu-projeto/src/components/Form.js
+++ meu-projeto/src/components/Form.js
@@ -1,16 +1,26 @@
+import { useState } from 'react';
+
 function Form(){
 
     function cadastrarUsuario(e){
         e.preventDefault();
-        console.log("Cadastrou o usuário!");
+        console.log(`Cadastrou o usuário "${name}" com a senha "${password}"`);
     }
+
+    const [name, setName] = useState('Michael'); // valor default
+    const [password, setPassword] = useState();
 
     return(
         <div>
             <h1>Meu cadastro:</h1>
             <form onSubmit={cadastrarUsuario}>
                 <div>
-                    <input type="text" placeholder="Digite o seu nome"/>
+                    <label htmlFor="name">Nome: </label>
+                    <input type="text" id="name" name="name" placeholder="Digite o seu nome" required value={name} onChange={(e) => setName(e.target.value)}/>
+                </div>
+                <div>
+                    <label htmlFor="password">Senha: </label>
+                    <input type="password" id="password" name="password" placeholder="Digite a sua senha" required onChange={(e) => setPassword(e.target.value)}/>
                 </div>
                 <div>
                     <input type="submit" value="Cadastrar"/>
